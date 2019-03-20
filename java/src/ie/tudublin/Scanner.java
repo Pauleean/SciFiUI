17376f984bd03c3d2ad3b8e84b98d96a9cc52b66
Niall <C17511003@mydit.ie>
2019-03-20 12:31:05 +0000

    changed scanner design

--- java/src/ie/tudublin/Scanner.java
+++ java/src/ie/tudublin/Scanner.java
@@ -30,15 +30,25 @@ public class Scanner
         ui.stroke(0, 150, 0);
         ui.ellipse(x, y, 2*rad, 2*rad);
         ui.fill(0, 150 , 0);
-        for(int i=0; i<8; i++)
+
+        //draw crosshair
+        for(int i=0; i<4; i++)
         {
-            ui.line((float)(x + (rad * Math.sin(i/5.0f))), (float)(y - (rad * Math.cos(i/5.0f))), (float)(x + (0.9f * (rad * Math.sin(i/5.0f)))), (float)(y - (0.9f * (rad * Math.cos(i/5.0f)))));
+            float pos = UI.map(i, 0, 4, 0, 6.28f);
+            ui.line((float)(x + (rad * Math.sin(pos))), (float)(y - (rad * Math.cos(pos))), (float)(x + (0.25f * (rad * Math.sin(pos)))), (float)(y - (0.25f * (rad * Math.cos(pos)))));
         }
 
-        ui.stroke(0, 0, 150);
+        //draw notches
+        for(int i=0; i<10; i++)
+        {
+            float pos = UI.map(i, -1, 10, 0, 1.57f);
+            ui.line((float)(x + (rad * Math.sin(pos))), (float)(y - (rad * Math.cos(pos))), (float)(x + (0.9f * (rad * Math.sin(pos)))), (float)(y - (0.9f * (rad * Math.cos(pos)))));
+        }
+
+        //draw status bar
         if(percent < 100)
         {
-            ui.arc(x, y, rad/3.0f, rad/3.0f, 0.0f, UI.map(percent, 0.0f, 100.0f, 0.0f, 6.28f));
+            ui.arc(x, y, rad/2.0f, rad/1.5f, 0.0f, UI.map(percent, 0.0f, 100.0f, 0.0f, 6.28f));
         }
         else
         {
