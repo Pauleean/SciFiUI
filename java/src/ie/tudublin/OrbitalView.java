1cc5faf643dfd9b0cb0fd94591da7f30b560493e
Niall <C17511003@mydit.ie>
2019-04-29 15:35:28 +0100

    Added code to draw grid around Single planet in seperate sketch

--- java/src/ie/tudublin/OrbitalView.java
+++ java/src/ie/tudublin/OrbitalView.java
@@ -13,7 +13,35 @@ public class OrbitalView extends PApplet
 
     public void settings()
     {
-        size(600, 400);
+        size(500, 500);
     }
 
+    public void setup()
+    {
+        stroke(0, 150, 0);
+        noFill();
+    }
+
+    public void draw()
+    {
+        background(0);
+        image(planet.getImg(), 150, 0, 350, 350);
+        drawGrid(150, 0, 350, 12);
+    }
+
+    public void drawGrid(float x, float y, float len, float num)
+    {
+        strokeWeight(2);
+        rect(x, y, len, len);
+        strokeWeight(1);
+
+        for(int i = 0; i < 12; i++)
+        {
+            float X = map(i, 0, num, x, x+len);
+            float Y = map(i, 0, num, y, y+len);
+
+            line(X, y, X, y+len);
+            line(x, Y, x+len, Y);
+        }
+    }
 }
